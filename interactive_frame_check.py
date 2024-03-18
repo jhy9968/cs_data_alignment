74606acdbeb3d4255fa0637424a882cb95d0c0a0
Haoyang Jiang <haoyang.jiang@monash.edu>
2024-03-18 20:43:29 +1100

    Added rear distance plot

--- interactive_frame_check.py
+++ interactive_frame_check.py
@@ -8,7 +8,7 @@ from scipy.signal import medfilt
 
 class Frame_check():
     def __init__(self, f, ffc) -> None:
-        self.folder_path = '/home/hjia0058/Documents/CS_data/{}/'.format(f)
+        self.folder_path = 'Z:/CS_data/{}/'.format(f)
         self.ffc = ffc
         self.auto_play = False
 
@@ -19,9 +19,11 @@ class Frame_check():
         self.distance_data = pd.read_csv(os.path.join(self.folder_path, 'Distance/distances.txt'), delimiter=', ', header=None)
         self.time_stamp = self.distance_data.iloc[:, 0].to_numpy()
         self.right_dist = self.distance_data.iloc[:, 1].to_numpy()
-        self.dist_check = self.distance_data.iloc[:, 2].to_numpy()
+        self.right_dist_check = self.distance_data.iloc[:, 2].to_numpy()
+        self.rear_dist = self.distance_data.iloc[:, 3].to_numpy()
+        self.rear_dist_check = self.distance_data.iloc[:, 4].to_numpy()
 
-        for i, check in enumerate(self.dist_check):
+        for i, check in enumerate(self.right_dist_check):
             if check == "OVER_LIMIT_ERR":
                 self.right_dist[i] = 4500
             elif check == "UNDER_LIMIT_ERR":
@@ -33,16 +35,28 @@ class Frame_check():
             elif check == "NO_DATA_ERR":
                 self.right_dist[i] = -200
 
-        # Process data
-        self.processed_right_dist = self.calculate_moving_average(self.right_dist, 10)
-        self.processed_right_dist = self.calculate_median_filter(self.processed_right_dist, 29)
+        for i, check in enumerate(self.rear_dist_check):
+            if check == "OVER_LIMIT_ERR":
+                self.rear_dist[i] = 4500
+            elif check == "UNDER_LIMIT_ERR":
+                self.rear_dist[i] = 30
+            elif check == "CHECKSUM_ERR":
+                self.rear_dist[i] = -30
+            elif check == "SERIAL_ERR":
+                self.rear_dist[i] = -100
+            elif check == "NO_DATA_ERR":
+                self.rear_dist[i] = -200
+
+        # # Process data
+        # self.processed_right_dist = self.calculate_moving_average(self.right_dist, 10)
+        # self.processed_right_dist = self.calculate_median_filter(self.processed_right_dist, 29)
 
         # Get file_names for front and rear image files
-        self.front_image_folder_path = os.path.join(self.folder_path, 'Images/Rear')
+        self.front_image_folder_path = os.path.join(self.folder_path, 'Images/Front')
         self.front_file_names = os.listdir(self.front_image_folder_path)
         self.front_file_names = [f for f in self.front_file_names if f.endswith('.jpg')]
 
-        self.rear_image_folder_path = os.path.join(self.folder_path, 'Images/Front')
+        self.rear_image_folder_path = os.path.join(self.folder_path, 'Images/Rear')
         self.rear_file_names = os.listdir(self.rear_image_folder_path)
         self.rear_file_names = [f for f in self.rear_file_names if f.endswith('.jpg')]
 
@@ -60,12 +74,13 @@ class Frame_check():
         self.axs[0, 0].plot(self.x, self.right_dist)
         self.axs[0, 0].set_xlabel('Time (s)')
         self.axs[0, 0].set_ylabel('Distance (mm)')
-        self.axs[0, 0].set_title('Original right distance')
+        self.right_title = self.axs[0, 0].set_title(f'Right distance: {self.right_dist[0]}mm')
 
         # Plot the second line
-        self.axs[1, 0].plot(self.processed_right_dist)
+        self.axs[1, 0].plot(self.x, self.rear_dist)
+        self.axs[1, 0].set_xlabel('Time (s)')
         self.axs[1, 0].set_ylabel('Distance (mm)')
-        self.axs[1, 0].set_title('Processed right distance')
+        self.rear_title = self.axs[1, 0].set_title(f'Rear distance: {self.rear_dist[0]}mm')
 
         # Show image
         self.index = 0
@@ -76,9 +91,11 @@ class Frame_check():
         self.axs[0, 1].set_title('Front camera image')
         self.axs[1, 1].set_title('Rear camera image')
 
-        # Add cursor marker
-        self.x_init, self.y_init = self.x[0], self.right_dist[0]  # Initial position for marker
-        self.marker, = self.axs[0, 0].plot(self.x_init, self.y_init, marker='o', color='red', markersize=5)
+        # Add cursor markers
+        # Initial position for marker in right dist plot
+        self.marker_right, = self.axs[0, 0].plot(self.x[0], self.right_dist[0], marker='o', color='red', markersize=5)
+        # Initial position for marker in rear dist plot
+        self.marker_rear, = self.axs[1, 0].plot(self.x[0], self.rear_dist[0], marker='o', color='red', markersize=5)
 
         plt.tight_layout()  # Adjust layout to prevent overlap
 
@@ -139,9 +156,9 @@ class Frame_check():
         
 
     def show_images(self, front_image, rear_image):
-        image_folder_path = os.path.join(self.folder_path, 'Images/Rear')
-        self.image1 = plt.imread(os.path.join(image_folder_path, front_image))
         image_folder_path = os.path.join(self.folder_path, 'Images/Front')
+        self.image1 = plt.imread(os.path.join(image_folder_path, front_image))
+        image_folder_path = os.path.join(self.folder_path, 'Images/Rear')
         self.image2 = plt.imread(os.path.join(image_folder_path, rear_image))
 
         # Flip front camera
@@ -160,14 +177,24 @@ class Frame_check():
         self.axs[1, 1].set_title('Rear camera image')
     
 
+    def update_markers(self):
+        # For right marker
+        y_clicked = self.right_dist[self.index]
+        self.marker_right.set_data(self.x[self.index], y_clicked)
+        self.right_title.set_text(f'Right distance: {y_clicked}mm')
+        # For rear marker
+        y_clicked = self.rear_dist[self.index]
+        self.marker_rear.set_data(self.x[self.index], y_clicked)
+        self.rear_title.set_text(f'Rear distance: {y_clicked}mm')
+
     def on_click(self, event):
-        if event.inaxes == self.axs[0, 0]:
+        if event.inaxes == self.axs[0, 0] or event.inaxes == self.axs[1, 0]:
             x_clicked = event.xdata
             print(f"Clicked on x = {x_clicked}")
-            # Update marker position
+            # Update index
             self.index = np.argmin(np.abs(self.x - x_clicked))
-            y_clicked = self.right_dist[self.index]
-            self.marker.set_data(self.x[self.index], y_clicked)
+            # Update markers
+            self.update_markers()
             plt.draw()
             # Show image
             front_image, rear_image = self.find_image(self.time_stamp[int(x_clicked*self.distance_frame_rate)])
@@ -179,15 +206,16 @@ class Frame_check():
             plt.close()  # Close the figure if 'q' is pressed
         elif event.key == 'a':
             self.index -= 1
-            y_clicked = self.right_dist[self.index]
-            self.marker.set_data(self.x[self.index], y_clicked)
+            self.update_markers()
             plt.draw()
             front_image, rear_image = self.find_image(self.time_stamp[int(self.x[self.index]*self.distance_frame_rate)])
             if front_image == self.front_image:
-                self.marker.set_color('r')
+                self.marker_right.set_color('r')
+                self.marker_rear.set_color('r')
                 print("Same image")
             else:
-                self.marker.set_color((0, 0.8, 0))
+                self.marker_right.set_color((0, 0.8, 0))
+                self.marker_rear.set_color((0, 0.8, 0))
                 self.front_image = front_image
                 self.rear_image = rear_image
                 print(front_image, rear_image)
@@ -196,15 +224,16 @@ class Frame_check():
                 self.show_images(front_image, rear_image)
         elif event.key == 'd':
             self.index += 1
-            y_clicked = self.right_dist[self.index]
-            self.marker.set_data(self.x[self.index], y_clicked)
+            self.update_markers()
             plt.draw()
             front_image, rear_image = self.find_image(self.time_stamp[int(self.x[self.index]*self.distance_frame_rate)])
             if front_image == self.front_image:
-                self.marker.set_color('r')
+                self.marker_right.set_color('r')
+                self.marker_rear.set_color('r')
                 print("Same image")
             else:
-                self.marker.set_color((0, 0.8, 0))
+                self.marker_right.set_color((0, 0.8, 0))
+                self.marker_rear.set_color((0, 0.8, 0))
                 self.front_image = front_image
                 self.rear_image = rear_image
                 print(front_image, rear_image)
